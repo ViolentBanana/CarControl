b921d87ccee70b814bfbee193ef441514642c120
CHEN <chenfangxu@keepyoga.com>
2025-06-03 10:22:45 +0800

    Initial Commit

---
+++ CarControl/CarControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarControlApp.swift
+//  CarControl
+//
+//  Created by CHEN on 2025/6/3.
+//
+
+import SwiftUI
+
+@main
+struct CarControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarControl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CarControl
+//
+//  Created by CHEN on 2025/6/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
